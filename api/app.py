8a5237ece578dcf7ad1e033fc532f040b71c8fde
Alexandr Bogomyakov <abogomyakov@iponweb.net>
2020-02-12 09:21:41 +0300

    Change project tree.

---
+++ api/app.py
@@ -0,0 +1,114 @@
+import os
+import json
+import logging
+from flask_cors import CORS
+from flask import Response, jsonify, send_from_directory, render_template, request, Flask, send_file
+from flask import flash, redirect, url_for
+from werkzeug.utils import secure_filename
+from database import DataBase
+
+HOME_DIR = os.path.dirname(os.path.realpath(__file__))
+DB = DataBase(
+    scheme=os.path.join(HOME_DIR, 'mods.sql'),
+    basefile=os.path.join(HOME_DIR, 'data.sqlite'))
+STATIC_DIR = os.path.join(HOME_DIR, '..', 'web', 'build')
+UPLOAD_FOLDER = os.path.join(HOME_DIR, 'storage')
+TMP_PATH = os.path.join(UPLOAD_FOLDER, 'tmp')
+MOD_PATH = os.path.join(UPLOAD_FOLDER, 'mods')
+ALLOWED_EXTENSIONS = set(['mod', 'xm', 'it', 's3m'])
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+log = logging.getLogger('hexound_api')
+#logging.getLogger("urllib3.connectionpool").setLevel(logging.WARNING)
+
+app = Flask(
+    __name__,
+    static_folder=os.path.realpath(STATIC_DIR))
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+# upload mechanics
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    message = request.args.get('message',
+        default='Choose file to upload',
+        type = str)
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            log.info("No file part")
+            return redirect(url_for('upload_file',
+                                    message="No file part, bad request."))
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            log.info("No selected file")
+            return redirect(url_for('upload_file',
+                                    message="No selected file"))
+        if file and allowed_file(file.filename):
+            log.info(f"Going to upload {file.filename}")
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(MOD_PATH, filename))
+            DB.add_mod(file.filename, file.mimetype)
+            return redirect(url_for('upload_file',
+                                    message="File uploaded."))
+        elif not allowed_file(file.filename):
+            return redirect(url_for('upload_file',
+                                    message=f"{file.mimetype} not allowed."))
+    return f'''
+    <!doctype html>
+    {message}
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+# NEED TO FIX IT!
+# Custom static data
+@app.route('/static/<path:filename>')
+def custom_static(filename):
+    return send_from_directory(os.path.join(STATIC_DIR, 'static'), filename)
+
+# Custom static data
+@app.route('/chiptune2.js/<path:filename>')
+def chiptune2_js(filename):
+    return send_from_directory(os.path.join(STATIC_DIR, 'chiptune2.js'), filename)
+
+# Custom static data
+@app.route('/libopenmpt.js.mem')
+def chiptune2_mem():
+    return send_from_directory(os.path.join(STATIC_DIR), 'libopenmpt.js.mem')
+
+# Custom static data
+@app.route('/mod/<path:filename>')
+def send_mod(filename):
+    return send_from_directory(MOD_PATH, filename)
+
+@app.route("/mods")
+def mods():
+    mods = None
+    # set limits and offset of query
+    limit = request.args.get('limit', default = 20, type = int)
+    offset = request.args.get('offset', default = 0, type = int)
+    mods = DB.get_mods(limit=limit, offset=offset)
+    return jsonify(mods)
+
+def main():
+    CORS(app)
+    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == '__main__':
+    main()
---
+++ api/database.py
@@ -0,0 +1,136 @@
+#
+# Copyright (c) 2020, UltraDesu <ultradesu@hexor.ru>
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#     * Neither the name of the <organization> nor the
+#     names of its contributors may be used to endorse or promote products
+#     derived from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY UltraDesu <ultradesu@hexor.ru> ''AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL UltraDesu <ultradesu@hexor.ru> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+"""
+.. module:: models
+   :synopsis: Contains database action primitives.
+.. moduleauthor:: AB <github.com/house-of-vanity>
+"""
+
+import sqlite3
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+log = logging.getLogger(__name__)
+
+
+# class DataBase create or use existent SQLite database file. It provides 
+# high-level methods for database.
+class DataBase:
+    """This class create or use existent SQLite database file. It provides 
+    high-level methods for database."""
+    def __init__(self, scheme, basefile='data.sqlite'):
+        """
+          Constructor creates new SQLite database if 
+          it doesn't exist. Uses SQL code from file for DB init.
+          :param scheme: sql filename
+          :type scheme: string
+          :return: None
+        """
+        self.scheme = ''
+        self.basefile = basefile
+        try:
+            conn = self.connect(basefile=basefile)
+        except:
+            log.debug('Could not connect to DataBase.')
+            return None
+        with open(scheme, 'r') as scheme_sql:
+            sql = scheme_sql.read()
+            self.scheme = sql
+            if conn is not None:
+                try:
+                    cursor = conn.cursor()
+                    cursor.executescript(sql)
+                except Exception as e:
+                    log.debug('Could not create scheme - %s', e)
+            else:
+                log.debug("Error! cannot create the database connection.")
+        log.info('DB created.')
+        self.close(conn)
+
+    def connect(self, basefile):
+        """
+          Create connect object for basefile
+          :param basefile: SQLite database filename
+          :type basefile: string
+          :return: sqlite3 connect object
+        """
+        #log.debug("Open connection to %s", basefile)
+        return sqlite3.connect(basefile, check_same_thread=False)
+
+    def execute(self, sql):
+        """
+          Execute SQL code. First of all connect to self.basefile. Close 
+          connection after execution.
+          :param sql: SQL code
+          :type sql: string
+          :return: list of response. Empty list when no rows are available.
+        """
+        conn = self.connect(basefile=self.basefile)
+        log.debug("Executing: %s", sql)
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        conn.commit()
+        result = cursor.fetchall()
+        self.close(conn)
+        return result
+
+    def close(self, conn):
+        """
+          Close connection object instance.
+          :param conn: sqlite3 connection object
+          :type conn: object
+          :return: None
+        """
+        #log.debug("Close connection to %s", self.basefile)
+        conn.close()
+
+    def add_mod(self, name, mime, author=None):
+        if author:
+            sql = f"INSERT OR IGNORE INTO mods('name', 'mime', 'author') VALUES ('{name}', '{mime}', '{author}')"
+        else:
+            sql = f"INSERT OR IGNORE INTO mods('name', 'mime') VALUES ('{name}', '{mime}')"
+        self.execute(sql)
+        return True
+
+    def get_mods(self, limit, offset):
+        sql = f"SELECT rowid, *, strftime('%s', date) FROM mods LIMIT {offset},{limit}"
+        mods = list()
+        result = self.execute(sql)
+        for mod in result:
+            mods.append(
+                {
+                    'id': mod[0],
+                    'filename': mod[1].replace(' ', '_'),
+                    'mimetype': mod[2],
+                    'str_time': mod[3],
+                    'author': mod[4],
+                    'time': mod[5],
+                }
+            )
+        return mods
