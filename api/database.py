954019d01711d20eb7359f1216e81f1d0256d434
xelnagamex <xelnagamex@gmail.com>
2020-02-16 18:16:20 +0300

    add find_mod method
    Returns true if given mod found in existing database, false otherwise

--- api/database.py
+++ api/database.py
@@ -134,3 +134,14 @@ class DataBase:
                 }
             )
         return mods
+   
+    def find_mod(self, filename):
+        """
+        Returns true if given mod found in existing database
+        TODO: Check new file contents checksum in the same way
+        """
+        sql = f"SELECT filename FROM mods"
+        result = self.execute(sql)
+        if filename in result:
+            return True
+        return False
