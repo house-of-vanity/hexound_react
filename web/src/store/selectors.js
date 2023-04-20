e42ea2bdd9d9d9c80ed62d094e41426d98b38291
Boris Cherepanov <cherr_guw@mail.ru>
2023-04-20 03:02:47 +0300

    feat: update deps

--- web/src/store/selectors.js
+++ web/src/store/selectors.js
@@ -1,14 +1,11 @@
-import { createSelector } from 'reselect'
-// import { fp } from 'lodash' 
+import { createSelector } from "@reduxjs/toolkit";
 
-const getTrackList = state => state.playerData.trackList
+const getTrackList = (state) => state.playerData.trackList;
 
-export const getArrTracks = createSelector(
-    [getTrackList],
-    (trackObject) => Object.values(trackObject)
-)
+export const getArrTracks = createSelector([getTrackList], (trackObject) =>
+	Object.values(trackObject)
+);
 
-export const getPlayList = createSelector(
-    [getTrackList],
-    (trackList) => Object.keys(trackList)
-)
+export const getPlayList = createSelector([getTrackList], (trackList) =>
+	Object.keys(trackList)
+);
